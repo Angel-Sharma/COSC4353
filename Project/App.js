1572f5524cfcf85f793f632d9d2ef242757007de
MeaslyFire0 <MeaslyFire0@gmail.com>
2024-03-20 17:22:22 -0500

    This is connecting the login module and registration page, its still WIP but its on its way to completetion soon

--- Project/App.js
+++ Project/App.js
@@ -1,11 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path')
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.send();
+const users = [
+  { username: 'user1', password: 'password1' },
+  { username: 'user2', password: 'password2' },
+  // Add more users as needed
+];
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'resources', 'login.html'));
 });
+
+app.post('/login',(req,res) => {
+  const {username, password} = req.body;
+
+  //Check if username and password are entered
+  if(!username || !password){
+    return res.status(400).json({error: 'Username and Password are required'});
+  }
+
+  // check if username and password are correct
+  const user = users.find(u => u.username === username && u.password === password);
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+  res.status(200).json({ message: 'Login successful', user });
+});
+
+app.post('/register',(req,res) => {
+  const {username, password } = req.body;
+
+    // Check if username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+      // Check if the user already exists in the database
+    const existingUser = users.find(u => u.username === username);
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    users.push({ username, password });
+    res.status(201).json({ message: 'Registration successful' });
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
---
+++ Project/readme.md
@@ -0,0 +1,17 @@
+# Fuel Quotes
+
+This project is a Node.js application for getting fuel pricing for a client
+
+## Installation
+
+Before running the application, make sure you have Node.js and npm (Node Package Manager) installed on your system.
+
+### Dependencies
+
+- [Express](https://expressjs.com/): Fast, unopinionated, minimalist web framework for Node.js.
+- [body-parser](https://www.npmjs.com/package/body-parser): Node.js body parsing middleware.
+
+To install the dependencies, navigate to the project directory in your terminal and run:
+
+```bash
+npm install express body-parser
